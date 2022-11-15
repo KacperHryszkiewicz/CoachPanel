5a60b1d470248320bd8e39b7ad3d3662a505b2a0
KacperHryszkiewicz <Kacper.Hryszkiewicz1999@gmail.com>
2022-11-15 14:23:22 +0100

    Initial commit

---
+++ src/main/java/com/hryszkiewicz/CoachPanel/CoachPanelApplication.java
@@ -0,0 +1,13 @@
+package com.hryszkiewicz.CoachPanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachPanelApplication.class, args);
+	}
+
+}
